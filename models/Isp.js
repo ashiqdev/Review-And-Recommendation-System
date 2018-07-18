5538491a52b6f5dd2f6d4276e708bee50669227a
Ashik <ashiquejava@gmail.com>
2018-07-18 19:32:16 +0600

    Bug fixed in Isp model

--- models/Isp.js
+++ models/Isp.js
@@ -14,29 +14,30 @@ const IspSchema = new mongoose.Schema({
     type: String,
     trim: true,
     required: 'Please enter coverage area',
-    created: {
-      type: Date,
-      default: Date.now,
-    },
-
-    location: {
-      type: {
-        type: String,
-        default: 'Point',
-      },
-      coordinates: [
-        {
-          type: Number,
-          required: 'You must supply coordinates!',
-        },
-      ],
-      address: {
-        type: String,
-        required: 'You must supply an address!',
-      },
-    },
-    photo: String,
   },
+  created: {
+    type: Date,
+    default: Date.now,
+  },
+
+  location: {
+    type: {
+      type: String,
+      default: 'Point',
+    },
+    coordinates: [
+      {
+        type: Number,
+        required: 'You must supply coordinates!',
+      },
+    ],
+    address: {
+      type: String,
+      required: 'You must supply an address!',
+    },
+  },
+
+  photo: String,
   packageDetails: [
     {
       type: String,
