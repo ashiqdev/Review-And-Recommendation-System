ef69ef9e9631e6583aa849812fd3064ac7c5cf08
Ashik <ashiquejava@gmail.com>
2018-07-20 14:01:39 +0600

    Store form route is updated

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,16 @@ const router = express.Router();
 const { catchErrors } = require('../handlers/errorHandlers');
 
 const storeController = require('../controllers/storeController');
-const ispController = require('../controllers/ispController');
+// const ispController = require('../controllers/ispController');
 
 // router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/isps', catchErrors(ispController.getIsps));
+// router.get('/isps', catchErrors(ispController.getIsps));
 
 // শুধু স্টোর মালিক দের জন্য
 router.get('/stores/add', storeController.addStore);
 // শুধু স্টোর মালিক দের জন্য
-router.get('/isps/add', ispController.addIsp);
+// router.get('/isps/add', ispController.addIsp);
 
 // ইনিশিয়ালি আমরা(ডেভেলপার) স্টোর সাবমিট এর জন্য ব্যবহার করব।
 router.post(
@@ -24,36 +24,36 @@ router.post(
   catchErrors(storeController.createStore)
 );
 // ইনিশিয়ালি আমরা(ডেভেলপার) স্টোর সাবমিট এর জন্য ব্যবহার করব।
-router.post('/isps/add', ispController.upload, catchErrors(ispController.resize), catchErrors(ispController.createIsp));
+// router.post('/isps/add', ispController.upload, catchErrors(ispController.resize), catchErrors(ispController.createIsp));
 
 // শুধু স্টোর মালিক দের জন্য
 router.get('/stores/:id/edit', catchErrors(storeController.editStore));
-router.get('/isps/:id/edit', catchErrors(ispController.editIsp));
+// router.get('/isps/:id/edit', catchErrors(ispController.editIsp));
 
 // শুধু স্টোর মালিক দের জন্য
 router.post(
-  '/add/:id',
+  '/stores/add/:id',
   storeController.upload,
   catchErrors(storeController.resize),
   catchErrors(storeController.updateStore)
 );
 
 // শুধু স্টোর মালিক দের জন্য
-router.post(
-  '/isps/add/:id',
-  storeController.upload,
-  catchErrors(storeController.resize),
-  catchErrors(ispController.updateIsp)
-);
+// router.post(
+//   '/isps/add/:id',
+//   storeController.upload,
+//   catchErrors(storeController.resize),
+//   catchErrors(ispController.updateIsp)
+// );
 
 // get store by their slug
 router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
-router.get('/isp/:slug', catchErrors(ispController.getIspBySlug));
+// router.get('/isp/:slug', catchErrors(ispController.getIspBySlug));
 
 // get stores by their tag
 router.get('/stores/tags', catchErrors(storeController.getStoresByTag));
 router.get('/stores/tags/:tag', catchErrors(storeController.getStoresByTagName));
-router.get('/isps/tags', catchErrors(ispController.getIspsByTag));
-router.get('/isps/tags/:tag', catchErrors(ispController.getIspsByTagName));
+// router.get('/isps/tags', catchErrors(ispController.getIspsByTag));
+// router.get('/isps/tags/:tag', catchErrors(ispController.getIspsByTagName));
 
 module.exports = router;
