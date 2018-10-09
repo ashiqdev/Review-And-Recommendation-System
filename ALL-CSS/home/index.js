2a2d659f42943cb74037997c7ef9536c93468709
Ashik <ashiquejava@gmail.com>
2018-10-09 22:11:57 +0600

    Landing page added and tags page updated

---
+++ ALL-CSS/home/index.js
@@ -0,0 +1,10 @@
+// var dice = Math.round(Math.random() * 3 + 1);
+// console.log(dice);
+// let image = document.querySelector('.bimage__section')
+
+// window.addEventListener('load', function () {
+//   image.style.display = 'block';
+//   var urlString = 'url(../images/' + dice + '.jpg)';
+//   image.style.backgroundImage = urlString;
+
+// })
